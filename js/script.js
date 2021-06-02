fb74d9f4faadb9d6084ebf714b7374368e775609
Kireeva <kireeva.en@defo.ru>
2021-06-02 17:15:02 +0300

    правки в дизайне

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ var carousel = new bootstrap.Carousel(myCarousel, {
 	interval: false,
 	touch: true
 });
+
+
+var myCarousel1 = document.querySelector('#carousel')
+var carousel1 = new bootstrap.Carousel(myCarousel1, {
+	interval: 2000
+});
